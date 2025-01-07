33eb1771619b778989a46eb19ddfde4d27c5e832
darwin <dkim10@cps.edu>
2025-01-07 10:39:55 -0600

    day 2

--- mainCode.py
+++ mainCode.py
@@ -6,6 +6,38 @@ from sklearn.model_selection import train_test_split
 
 data=pd.read_csv("AirQualityUCI.csv", sep=";")
 print(data)
-x = data["C6H6(GT)"]
-y = [data["Date"],data["Time"]]
-print(x,y)
+x = data["Date"]
+y = data["NO2(GT)"]
+years=[]
+months=[]
+days2004=[]
+days2005=[]
+xSplit=[]
+Data2004=[]
+Data2005=[]
+for i in range(len(y)):
+    xSplit.append(x[i].split("/"))
+for i in range(len(xSplit)):
+    #print(data["Time"][i],ySplit[i][1])
+    if data["Time"][i]=="04.00.00" and int(xSplit[i][1])==3 and int(xSplit[i][2])==2004:
+        Data2004.append(y[i])
+        days2004.append(int(xSplit[i][0]))
+    if data["Time"][i]=="04.00.00" and int(xSplit[i][1])==3 and int(xSplit[i][2])==2005:
+        Data2004.append(y[i])
+        days2004.append(int(xSplit[i][0]))
+print(Data2004)
+print(days2004)
+fig,graph = plt.subplots(2)
+graph[0].scatter(Data2004,days2004)
+graph[0].set_xlabel("Nitrogen Dioxide concentration (ppm)")
+graph[0].set_ylabel("Day of Month")
+graph[1].scatter(Data2005,days2005)
+graph[1].set_xlabel("Nitrogen Dioxide concentration (ppm)")
+graph[1].set_ylabel("Day of Month")
+
+plt.tight_layout()
+plt.show()
+
+
+
+

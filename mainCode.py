0a8e13a06f9be067098a4c24fbae0b22b26e47c4
darwin <dkim10@cps.edu>
2025-01-10 09:59:24 -0600

    day 4 (had to recommit because I forgot to sync)

--- mainCode.py
+++ mainCode.py
@@ -9,33 +9,34 @@ print(data)
 x = data["Date"]
 y = data["NO2(GT)"]
 years=[]
-days=[[]]
+days=[[],[],[],[],[],[],[],[],[],[],[],[],[]]
 xSplit=[]
-Data=[[]]
+Data=[[],[],[],[],[],[],[],[],[],[],[],[],[]]
 colors=["r",'g','b','c','m','y']
 for i in range(len(y)):
     xSplit.append(x[i].split("/"))
 fig, graph = plt.subplots(4)
 
-for m in range(12):
+for m in range(1,13):
     for i in range(len(xSplit)):
         #print(data["Time"][i],ySplit[i][1])
-        if data["Time"][i]=="04.00.00" and int(xSplit[i][1])==m and int(xSplit[i][2])==2005:
+        if data["Time"][i]=="04.00.00" and int(xSplit[i][1])==m and data["NO2(GT)"]!=-200:
             Data[m].append(y[i])
             days[m].append(int(xSplit[i][0]))
-    if m>11:
-        graph[0].scatter(days[m],Data[m],label="Winter",c=colors[m-12])
+    if m==12:
+        graph[0].scatter(days[m],Data[m],c=colors[m-12])
     if 0<m<3:
-        graph[0].scatter(days[m],Data[m],label="Winter",c=colors[m+1])
+        graph[0].scatter(days[m],Data[m],c=colors[m])
     if 3<m<6:
-        graph[1].scatter(days[m],Data[m],label="Spring",c=colors[m-3])
-    if 6<m<8:
-        graph[2].scatter(days[m],Data[m],label='Summer',c=colors[m-6])
-    if 8<m<12:
-        graph[3].scatter(days[m],Data[m],label='Fall',c=colors[m-8])
+        graph[1].scatter(days[m],Data[m],c=colors[m-3])
+    if 6<m<9:
+        graph[2].scatter(days[m],Data[m],c=colors[m-6])
+    if 9<m<12:
+        graph[3].scatter(days[m],Data[m],c=colors[m-9])
     for i in range(4):
         graph[i].set_xlabel("Day of month")
         graph[i].set_ylabel("Concentration of NO2(ppm)")
+
 plt.show()
 
 
